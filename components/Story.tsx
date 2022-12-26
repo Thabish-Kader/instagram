ebeb60219133cace755a621e48b26cd94018815f
Thabish <kadertabish2@gmail.com>
2022-12-26 10:56:27 +0400

    status-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ components/Story.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+const Story = (props: Props) => {
+	return <div>Story</div>;
+};
---
+++ typongs.d.ts
@@ -0,0 +1,9 @@
+type Sugesstion = {
+	userId: string;
+	username: string;
+	email: string;
+	avatar: string;
+	birthdate: Date;
+	password: string;
+	registeredAt: Date;
+};
