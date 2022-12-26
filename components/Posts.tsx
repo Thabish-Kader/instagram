221d7b0429b72b2715571b6d360eb6ef861c6239
Thabish <kadertabish2@gmail.com>
2022-12-26 11:06:42 +0400

    singleposts-typings-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ components/Posts.tsx
@@ -0,0 +1,25 @@
+import { SinglePost } from "./SinglePost";
+
+export const Posts = () => {
+	const posts = [
+		{
+			id: "123",
+			username: "byteBattalion",
+			img: "https://my.kumonglobal.com/wp-content/uploads/2022/03/Learn-from-Rowan-Atkinson_Kumon-Malaysia_530x530_NewsThumbnail.jpg",
+			caption: "Join me on cartton Newtwoek",
+		},
+	];
+	return (
+		<div>
+			{posts.map((post) => (
+				<SinglePost
+					key={post.id}
+					id={post.id}
+					username={post.username}
+					img={post.img}
+					caption={post.caption}
+				/>
+			))}
+		</div>
+	);
+};
--- components/SinglePost.tsx
+++ components/SinglePost.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+type Props = {
+	id: string;
+	username: string;
+	img: string;
+	caption: string;
+};
 
-export const SinglePost = () => {
+export const SinglePost = ({ id, username, img, caption }: Props) => {
 	return <div>SinglePost</div>;
 };
