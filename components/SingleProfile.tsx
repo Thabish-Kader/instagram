405f322069a5f9ac233bf1364901ffe5bb4558c4
Thabish <kadertabish2@gmail.com>
2022-12-27 10:21:33 +0400

    singleprofiles-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- components/SingleProfile.tsx
+++ components/SingleProfile.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const SingleProfile = () => {
 	return <div>SingleProfile</div>;
 };
--- components/TopProfiles.tsx
+++ components/TopProfiles.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
+import { SingleProfile } from "./SingleProfile";
 
 export const TopProfiles = () => {
-	return <div>TopProfiles</div>;
+	const [profiles, setProfiles] = useState<UserData[]>([]);
+
+	useEffect(() => {
+		const profiles = [...Array(5)].map((_, i) => ({
+			userId: faker.datatype.uuid(),
+			username: faker.internet.userName(),
+			email: faker.internet.email(),
+			avatar: faker.image.avatar(),
+			password: faker.internet.password(),
+			birthdate: faker.date.birthdate(),
+			registeredAt: faker.date.past(),
+		}));
+		setProfiles(profiles);
+	}, []);
+
+	return (
+		<div className="mt-4 ml-10">
+			<div className="flex justify-between text-sm mb-5">
+				<h3 className="text-sm font-bold text-gray-400">
+					Suggestions for you
+				</h3>
+				<button className="text-gray-600 font-semibold">See All</button>
+			</div>
+			{profiles.map((profile) => (
+				<SingleProfile key={profile.userId} profile={profile} />
+			))}
+		</div>
+	);
 };
