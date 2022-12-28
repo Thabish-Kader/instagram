4df1517497c69360e947a7f0047f7dc37c2416b2
Thabish <kadertabish2@gmail.com>
2022-12-28 10:26:01 +0400

    ui-change-myProfile
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- components/MyProfile.tsx
+++ components/MyProfile.tsx
@@ -1,20 +1,26 @@
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import React from "react";
 
 export const MyProfile = () => {
+	const { data: session } = useSession();
 	return (
 		<div className="flex items-center justify-between mt-14 ml-10">
-			<div className="relative w-16 h-16">
-				<Image
-					className="rounded-full border p-[2px] "
-					src="https://my.kumonglobal.com/wp-content/uploads/2022/03/Learn-from-Rowan-Atkinson_Kumon-Malaysia_530x530_NewsThumbnail.jpg"
-					alt=""
-					fill
-				/>
-			</div>
+			{/* <div className="relative w-16 h-16"> */}
+			<Image
+				className="rounded-full border p-[2px] object-cover"
+				src={`${
+					session?.user?.image ||
+					"https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper.png"
+				}`}
+				alt="/user"
+				height={60}
+				width={60}
+			/>
+			{/* </div> */}
 
 			<div className="flex-1 mx-4">
-				<h2 className="font-bold ">byte</h2>
+				<h2 className="font-bold ">{session?.user?.username}</h2>
 				<h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
 			</div>
 
