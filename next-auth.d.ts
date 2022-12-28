90e653d2f6e15729739293d701e1928b8700cf0c
Thabish <kadertabish2@gmail.com>
2022-12-28 10:15:53 +0400

    next-auth-typings
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ next-auth.d.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+	/**
+	 * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+	 */
+	interface Session {
+		user: {
+			/** The user's name. */
+			name: string;
+			uid: string;
+			image: string;
+			username: string;
+			email: string;
+		};
+	}
+}
